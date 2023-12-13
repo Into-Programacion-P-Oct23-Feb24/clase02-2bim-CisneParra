23d829907dd91d89a18842b388f10be10c95582f
CisneParra <adparra2@utpl.edu.ec>
2023-12-13 16:16:47 -0500

    cambios de la clase

--- Ejemplos/src/paquete1/Ejemplo041.java
+++ Ejemplos/src/paquete1/Ejemplo041.java
@@ -9,26 +9,29 @@ package paquete1;
  *
  * @author reroes
  */
-public class Ejemplo04 {
+public class Ejemplo041 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
-        
+
         // arreglo de tipo entero
         //int[] arreglo1 = new int[5];
-         int[] arreglo1 = {1000, 1, 2, 2000, 300};
+        int[] arreglo1 = {1000, 1, 2, 2000, 300};
+        String acumulador = "";
         // System.out.println(arreglo1.length);
         //arreglo1[2] = 1000;
         for (int i = 0; i < arreglo1.length; i++) { //la palabra lenght cuando le agrego como caracteristica de un arreglo
-                                                    //me da el numero de elementos que tiene el arreglo
-            System.out.printf("Subíndice o Índice %d, con valor %d\n",
-                    i, arreglo1[i]);
+            //me da el numero de elementos que tiene el arreglo
+            acumulador = String.format("%sSubíndice o Índice %d, con valor %d\n",
+                    acumulador,
+                    i,
+                    arreglo1[i]);
         }
-        
-        
+        System.out.printf("%s", acumulador);
+
     }
 
 }
